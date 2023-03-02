2925d79107848771e23d6e2f58249e495267df7a
Aarjan <aarjan.pat@gmail.com>
2023-03-02 18:48:57 +0545

    crew component updated

--- one_piece/src/components/Crew.js
+++ one_piece/src/components/Crew.js
@@ -4,6 +4,11 @@ import brook from "./Images/brook.png"
 import chopper from "./Images/chopper.png"
 import zoro from "./Images/zoro.png"
 import sanji from "./Images/sanji.png"
+import robin from "./Images/robin.png"
+import franky from "./Images/franky.png"
+import usopp from "./Images/usopp.png"
+import nami from "./Images/nami.png"
+
 
 import React from 'react'
 
@@ -15,8 +20,12 @@ function Crew() {
             <img className="brook--img" src={brook}/>
             <img className="chopper--img" src={chopper}/>
             <img className="zoro--img" src={zoro}/>
+            <img className="nami--img" src={nami}/>
+            <img className="franky--img" src={franky}/>
+            <img className="usopp--img" src={usopp}/>
+            <img className="nami--img" src={nami}/>
+            <img className="robin--img" src={robin}/>
             <img className="sanji--img" src={sanji}/>
-
         </div>
     </div>
   )
