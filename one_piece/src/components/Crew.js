4e1bcf5b6c3c89683391889fff46784ef3f69fa8
Aarjan <aarjan.pat@gmail.com>
2023-03-02 19:36:44 +0545

    updated the crew component

--- one_piece/src/components/Crew.js
+++ one_piece/src/components/Crew.js
@@ -15,17 +15,29 @@ import React from 'react'
 function Crew() {
   return (
     <div className='crew'>
+
+        <div className='text'>
+            <div className='text-1'>
+                <h1>MEET THE</h1>
+            </div>
+            <div className='text-2'>
+                <h1>MUGIWARA</h1>
+            </div>
+            <div className='text-3'>
+                <h1>NO ICHIMI</h1>
+            </div>
+        </div>
         <div className='members'>
-            <img className="luffy--img" src={luffy}/>
             <img className="brook--img" src={brook}/>
             <img className="chopper--img" src={chopper}/>
-            <img className="zoro--img" src={zoro}/>
             <img className="nami--img" src={nami}/>
             <img className="franky--img" src={franky}/>
+            <img className="zoro--img" src={zoro}/>
+            <img className="luffy--img" src={luffy}/>
+            <img className="sanji--img" src={sanji}/>
             <img className="usopp--img" src={usopp}/>
             <img className="nami--img" src={nami}/>
             <img className="robin--img" src={robin}/>
-            <img className="sanji--img" src={sanji}/>
         </div>
     </div>
   )
