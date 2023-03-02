a69f1624860a86011ba7edae05e6ccbc96f699ff
Aarjan <aarjan.pat@gmail.com>
2023-03-02 18:39:56 +0545

    added crew members png

--- one_piece/src/components/Crew.js
+++ one_piece/src/components/Crew.js
@@ -1,11 +1,23 @@
 import './Crew.css'
+import luffy from "./Images/luffy.png"
+import brook from "./Images/brook.png"
+import chopper from "./Images/chopper.png"
+import zoro from "./Images/zoro.png"
+import sanji from "./Images/sanji.png"
 
 import React from 'react'
 
 function Crew() {
   return (
     <div className='crew'>
-        <h1></h1>
+        <div className='members'>
+            <img className="luffy--img" src={luffy}/>
+            <img className="brook--img" src={brook}/>
+            <img className="chopper--img" src={chopper}/>
+            <img className="zoro--img" src={zoro}/>
+            <img className="sanji--img" src={sanji}/>
+
+        </div>
     </div>
   )
 }
