10cefdf6cf724a85044d3370d1b5c92f8ca8722b
Reshav <thapareshav50@gmail.com>
2023-03-05 22:01:38 +0545

    sunny added in one piece

--- one_piece/src/components/One/One.jsx
+++ one_piece/src/components/One/One.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './One.css'
 import background from "../../Images/background.png"
 import logo from "../../Images/logo.png"
+import sunny from "../../Images/sunny.png"
 
 const One = () => {
   return (
@@ -14,6 +15,9 @@ const One = () => {
             <h1>ONE</h1>
             <img src={logo}/>
             <h2>PIECE</h2>
+          </div>
+          <div className='sunny'>
+            <img src = {sunny}/>
           </div>
         </section>
     </>
