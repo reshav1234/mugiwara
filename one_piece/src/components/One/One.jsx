a76bee06d06996c34ca27ca1a2489dfabe8cbb58
Reshav <thapareshav50@gmail.com>
2023-03-05 22:27:09 +0545

    cloud and some island has bee added

--- one_piece/src/components/One/One.jsx
+++ one_piece/src/components/One/One.jsx
@@ -3,6 +3,11 @@ import './One.css'
 import background from "../../Images/background.png"
 import logo from "../../Images/logo.png"
 import sunny from "../../Images/sunny.png"
+import cloud from '../../Images/cloud.png'
+import skypie from "../../Images/skypie.png"
+import island from "../../Images/island.png"
+import hill from '../../Images/hill.png'
+
 
 const One = () => {
   return (
@@ -19,6 +24,19 @@ const One = () => {
           <div className='sunny'>
             <img src = {sunny}/>
           </div>
+          <div className = "cloud">
+            <img src = {cloud}/>
+          </div>
+          <div className = "skypie">
+            <img src = {skypie}/>
+          </div>
+          <div className= 'hill'>
+            <img src = {hill}/>
+          </div>
+          <div className = "island">
+            <img src = {island}/>
+          </div>
+
         </section>
     </>
   )
