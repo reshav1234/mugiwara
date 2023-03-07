d89904893be14d77dd892d43b6177ab3f5dd8bd4
Reshav <thapareshav50@gmail.com>
2023-03-07 23:45:31 +0545

    luffy dir added

--- one_piece/src/App.js
+++ one_piece/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Crew from './components/Crew/Crew'
 import One from './components/One/One'
+import Luffy from './components/Luffy/Luffy';
 
 function App() {
   return (
     <div className="app">
       <One />
       <Crew />
+      <Luffy />
     </div>
   )
 }
---
+++ one_piece/src/components/Luffy/Luffy.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import badal from '../../Images/cloud_01.png'
+import luffy from '../../Images/luffy.png'
+import wheel from '../../Images/wheel.png'
+import wood from '../../Images/wood.png'
+import member from '../../Images/member.png'
+
+const Luffy = () => {
+  return (
+    <>
+        <section className="luffy">
+            <div className = "wood">
+                <img src = {wood} alt = "wood" />
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Luffy
