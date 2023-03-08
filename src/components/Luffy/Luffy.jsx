58646a91c9bef1d277b0a36780d8fac46ee2ef65
Reshav <thapareshav50@gmail.com>
2023-03-08 11:30:27 +0545

    luffy css left

--- src/components/Luffy/Luffy.jsx
+++ src/components/Luffy/Luffy.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import './Luffy.css'
+
+
 import badal from '../../Images/cloud_01.png'
-import luffy from '../../Images/luffy.png'
+import luffy from '../../Images/lufffy.png'
 import wheel from '../../Images/wheel.png'
 import wood from '../../Images/wood.png'
 import member from '../../Images/member.png'
@@ -9,8 +12,18 @@ const Luffy = () => {
   return (
     <>
         <section className="luffy">
+
             <div className = "wood">
                 <img src = {wood} alt = "wood" />
+            </div>
+            <div className = "member">
+                <img src = {member} alt = "member" />
+            </div>
+            <div className = "mugi">
+                <img src = {luffy} alt = "mugiwara" />
+            </div>
+            <div className = "description">
+                <h1>Monkey D Luffy</h1>
             </div>
         </section>
     </>
